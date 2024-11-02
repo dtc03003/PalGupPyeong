499bca3d4b536eabdb2d8d087e8d14f7feba3637
Meerkat <66206266+dtc03003@users.noreply.github.com>
2024-11-02 23:56:15 +0900

    feat: router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import GlobalStyle from "./styles/GlobalStyles";
+import theme from "./styles/theme";
+import Home from "./pages/Home";
+import LoginPage from "./pages/Login";
+import SignupPage from "./pages/Signup";
+
 function App() {
-  return <></>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
