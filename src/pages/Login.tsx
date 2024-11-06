42df0c8c1062584eff45328f194c88c96d917ed0
Meerkat <66206266+dtc03003@users.noreply.github.com>
2024-11-06 10:20:01 +0900

    feat: 로그인 회원가입 페이지

---
+++ src/pages/Login.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { login } from "../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      await login(email, password);
+      alert("로그인 성공");
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+      alert("로그인 실패");
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="이메일"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="비밀번호"
+      />
+      <button onClick={handleLogin}>로그인</button>
+    </div>
+  );
+};
+
+export default LoginPage;
