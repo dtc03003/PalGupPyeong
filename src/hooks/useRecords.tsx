85d4bfd0b61662853f9ad310df0330ba445763e4
Meerkat <66206266+dtc03003@users.noreply.github.com>
2024-11-15 02:42:39 +0900

    chore: react-query에서 @tanstack/react-query로 변경

--- src/hooks/useRecords.tsx
+++ src/hooks/useRecords.tsx
@@ -1,6 +1,6 @@
 import { collection, addDoc, getDocs } from "firebase/firestore";
 import { db } from "../api/firebase";
-import { useMutation, useQuery } from "react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 interface PushupRecord {
   count: number;
@@ -15,20 +15,25 @@ type RecordData = {
 
 // 기록 추가
 export const useAddRecord = () => {
-  return useMutation(async (newRecord: PushupRecord) => {
-    const docRef = await addDoc(collection(db, "pushupRecords"), newRecord);
-    return { id: docRef.id, ...newRecord };
+  return useMutation({
+    mutationFn: async (newRecord: PushupRecord) => {
+      const docRef = await addDoc(collection(db, "pushupRecords"), newRecord);
+      return { id: docRef.id, ...newRecord };
+    },
   });
 };
 
 // 기록 리스트 조회
 export const useRecords = () => {
-  return useQuery<RecordData[]>("pushupRecords", async () => {
-    const querySnapshot = await getDocs(collection(db, "pushupRecords"));
-    return querySnapshot.docs.map((doc) => ({
-      id: doc.id,
-      date: doc.data().date.toDate ? doc.data().date.toDate() : new Date(doc.data().date),
-      count: doc.data().count,
-    }));
+  return useQuery<RecordData[]>({
+    queryKey: ["pushupRecords"],
+    queryFn: async () => {
+      const querySnapshot = await getDocs(collection(db, "pushupRecords"));
+      return querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        date: doc.data().date.toDate ? doc.data().date.toDate() : new Date(doc.data().date),
+        count: doc.data().count,
+      }));
+    },
   });
 };
