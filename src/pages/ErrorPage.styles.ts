dacdb164bf15dfcbbbfcd6db28980b7c61218e8d
김동현 <dtc03003@naver.com>
2025-02-15 19:32:01 +0900

    feat: Error 페이지 구현

---
+++ src/pages/ErrorPage.styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const ErrorPageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 100px;
+    text-align: center;
+`;
+
+export const Title = styled.p`
+    font-size: 60px;
+    font-weight: bold;
+    margin: 20px 0;
+`;
+
+export const Description = styled.p`
+    font-size: 20px;
+    margin: 10px 0;
+`;
---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,14 @@
+import * as S from "./ErrorPage.styles";
+import pushupGif from "../assets/pushup.gif";
+
+const ErrorPage = () => {
+    return (
+        <S.ErrorPageContainer>
+            <img src={pushupGif} alt="팔굽혀펴기" />
+            <S.Title>404 ERROR</S.Title>
+            <S.Description>페이지를 찾을 수 없습니다.</S.Description>
+        </S.ErrorPageContainer>
+    );
+};
+
+export default ErrorPage;
