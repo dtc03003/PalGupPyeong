5402ea126d3804732d474476a311c33bde8c89f3
김동현 <dtc03003@naver.com>
2025-05-11 18:28:47 +0900

    feat: SkeletonRecordItem 컴포넌트 구현

---
+++ src/components/record/SkeletonRecordItem.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { SkeletonBox } from "@components/common/SkeletonBox";
+
+const SkeletonRecordItem = () => {
+  return (
+    <Container>
+      <SkeletonBox $width="30%" $height="20px" />
+      <SkeletonBox $width="80%" $height="14px" $margin="6px 0 0 0" />
+    </Container>
+  );
+};
+
+export default SkeletonRecordItem;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  padding: 12px;
+  border-bottom: 1px solid #eaeaea;
+`;
