5faba0fc6e9b91b96866eadc9d533df6e31cd675
김동현 <dtc03003@naver.com>
2025-05-12 17:42:12 +0900

    refactor: 드롭다운 메뉴를 탭 메뉴로 변경

---
+++ src/components/common/TabSelector.styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const TabWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  margin: 10px 0;
+`;
+
+export const TabButton = styled.button<{ active: boolean }>`
+  padding: 5px 10px;
+  background-color: ${({ active }) => (active ? "#ddd" : "#f9f9f9")};
+  border: 1px solid #ccc;
+  border-radius: 3.75px;
+  font-weight: ${({ active }) => (active ? "bold" : "normal")};
+  cursor: pointer;
+
+  &:hover {
+    background-color: #eee;
+  }
+`;
