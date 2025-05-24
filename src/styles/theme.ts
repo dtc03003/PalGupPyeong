482fc191da15187ed8a17669df580170fb185463
김동현 <dtc03003@naver.com>
2025-05-24 18:10:11 +0900

    feat: lightTheme, darkTheme 정의 및 AppTheme 타입 생성

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,14 +1,58 @@
-import { DefaultTheme } from "styled-components";
+export const lightTheme = {
+  mode: "light",
 
-const theme: DefaultTheme = {
-  colors: {},
+  // 배경 계층
+  outside: "#F4F4F4",
+  bg0: "#FFFFFF",
+  bg1: "#F5F5F5",
+  bg2: "#EAEAEA",
+  bg3: "#DDDDDD",
+  navBg: "#ff4b4b",
 
-  fontSizes: {
-    small: "12px",
-    medium: "16px",
-    large: "20px",
-    xlarge: "24px",
-  },
+  // 텍스트
+  text: "#333",
+  subText: "#555555",
+  strongText: "#000",
+  grayText0: "##555",
+  grayText1: "#666",
+
+  // 포인트
+  primary: "#007bff",
+  primaryText: "#FFFFFF",
+  primaryHover: "#0056b3",
+
+  // 기타
+  border: "#DADADA",
+  disabled: "#ccc",
 };
 
-export default theme;
+export const darkTheme = {
+  mode: "dark",
+
+  // 배경 계층
+  outside: "#000",
+  bg0: "#121212",
+  bg1: "#1E1E1E",
+  bg2: "#2A2A2A",
+  bg3: "#333333",
+  navBg: "	#e64545",
+
+  // 텍스트
+  text: "#FFFFFF",
+  subText: "#AAAAAA",
+  strongText: "#fff",
+  grayText0: "#AAA",
+  grayText1: "#888",
+
+  // 포인트
+  primary: "#339aff",
+  primaryText: "#FFFFFF",
+  primaryHover: "#0056b3",
+
+  // 기타
+  border: "#333333",
+  disabled: "#555555",
+};
+
+// 타입 추출
+export type AppTheme = typeof lightTheme;
