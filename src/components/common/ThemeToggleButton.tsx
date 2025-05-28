5e89bf0b17682c095fbc43ef82e404cd37289fb2
김동현 <dtc03003@naver.com>
2025-05-28 17:51:03 +0900

    refactor: ThemeToggleButton 이모지를 SVG 아이콘으로 변경

--- src/components/common/ThemeToggleButton.tsx
+++ src/components/common/ThemeToggleButton.tsx
@@ -1,11 +1,17 @@
 import { useThemeStore } from "@store/themeStore";
+import DarkModeIcon from "@assets/icons/icon_dark.svg?react";
+import LightModeIcon from "@assets/icons/icon_light.svg?react";
 
 const ThemeToggleButton = () => {
   const { theme, toggleTheme } = useThemeStore();
 
   return (
     <button onClick={toggleTheme}>
-      {theme === "light" ? "🌙 다크모드" : "☀️ 라이트모드"}
+      {theme === "light" ? (
+        <DarkModeIcon style={{ width: "25px", height: "25px" }} />
+      ) : (
+        <LightModeIcon style={{ width: "25px", height: "25px" }} />
+      )}
     </button>
   );
 };
