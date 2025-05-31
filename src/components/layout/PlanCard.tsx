2c28acb97a1139c9b14604e6b8331837e72ed7df
김동현 <dtc03003@naver.com>
2025-05-31 18:16:17 +0900

    feat: PlanCard 컴포넌트 구현

---
+++ src/components/layout/PlanCard.tsx
@@ -0,0 +1,25 @@
+import * as S from "./PlanCard.styles";
+
+interface PlanCardProps {
+  point: string;
+  title: string;
+  src: string;
+  imgPosition?: "left" | "right";
+}
+
+const PlanCard = ({
+  point,
+  title,
+  src,
+  imgPosition = "right",
+}: PlanCardProps) => {
+  return (
+    <S.Wrapper>
+      <S.Point>{point}</S.Point>
+      <S.Title>{title}</S.Title>
+      <S.Image src={src} alt="Plan Card Image" imgPosition={imgPosition} />
+    </S.Wrapper>
+  );
+};
+
+export default PlanCard;
